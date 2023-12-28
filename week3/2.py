7ea09bbd85030206ee89d0f80fb0da10e28db441
SangEon <86558575+sangeon-ahn@users.noreply.github.com>
2023-12-28 17:05:42 +0900

    2 solved

--- week3/2.py
+++ week3/2.py
@@ -1,7 +1,11 @@
-N = int(input())
-dp = [0] * (N + 1) # dp[i]: N = i일 때 만들 수 있는 2진수 개수
+import sys
+input = sys.stdin.readline
+
+N = int(input().rstrip())
+dp = [0] * (N + 10) # dp[i]: N = i일 때 만들 수 있는 2진수 개수
 # dp[i] = dp[i-1] + dp[i-2]
 
+dp[0] = 0
 dp[1] = 1
 dp[2] = 2
 
